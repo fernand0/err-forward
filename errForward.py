b89f830104de829f2272100ef5b7a3bc3e699207
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2017-11-11 11:56:10 +0000

    Publishing presence in channel

--- errForward.py
+++ errForward.py
@@ -1,7 +1,8 @@
 from errbot import BotPlugin, botcmd, webhook
 from slackclient import SlackClient
 import configparser
-import os
+import os, pwd
+import datetime
 
 
 class ErrForward(BotPlugin):
@@ -9,13 +10,18 @@ class ErrForward(BotPlugin):
     An Err plugin for forwarding instructions
     """
 
-    #def activate(self):
-    #    """
-    #    Triggers on plugin activation
+    def activate(self):
+        """
+        Triggers on plugin activation
 
-    #    You should delete it if you're not using it to override any default behaviour
-    #    """
-    #    super(Skeleton, self).activate()
+        You should delete it if you're not using it to override any default behaviour
+        """
+        #super(Skeleton, self).activate()
+        
+        dateNow = datetime.datetime.now().isoformat()
+        userName = pwd.getpwuid(os.getuid())[0]
+        userHost = os.uname()[1]
+        self.publishSlack("%s: Hello I'm User:%s at Host:%s" % (dateNow, userName, userHost))
 
     #def deactivate(self):
     #    """
@@ -42,6 +48,20 @@ class ErrForward(BotPlugin):
                 return self.config[option]
             else:
                 return None
+
+    def publishSlack(self, args):
+        config = configparser.ConfigParser()
+        config.read([os.path.expanduser('~/'+'.rssSlack')])
+    
+        slack_token = config["Slack"].get('api-key')
+        sc = SlackClient(slack_token)
+    
+        chan = "#" + str(self._check_config('channel'))
+        sc.api_call(
+              "chat.postMessage",
+               channel=chan,
+               text= args
+               )
 
     @botcmd
     def forward(self, mess, args):
