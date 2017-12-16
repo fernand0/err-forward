a1a8c4d098279e6266898abd46ed074ed598ab3f
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2017-12-17 00:13:14 +0100

    Fixing the place where we need to look for the sender

--- errForward.py
+++ errForward.py
@@ -152,11 +152,13 @@ class ErrForward(BotPlugin):
                     posMe = msg['text'].find(userName+'@'+userHost)
                     if (posMe >= 0):
                         # It's for me
+                        yield("posMe %s" % msg['text'][posMe:])
                         posIFrom = msg['text'].find('From', posMe)
                         self.log.info('Reply: %s' % msg['text'])
                         self.log.info('posIFrom %d' % posIFrom)
                         if posIFrom >= 0:
                             posFFrom = msg['text'].find('. ', posIFrom)
+                            yield("posIFrom %s" % msg['text'][posIFrom:posFFrom])
                         self.log.info('reply From %s' % msg['text'][posIFrom+5:posFFrom])
                         replies = msg['text'][pos+len('Rep:')+2:]
                         for reply in replies.split('\n'):
