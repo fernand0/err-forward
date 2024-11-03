315014429e9eae48dc6e812a7b2cb1e8c181079a
fernand0 <fernand0@elmundoesimperfecto.com>
2024-11-03 12:05:54 +0100

    Updated requirements for errbot

--- errForward.py
+++ errForward.py
@@ -12,7 +12,6 @@ from errbot import BotPlugin, botcmd, webhook
 from errbot.backends.base import Message, Identifier
 from errbot.templating import tenv
 
-import socialModules
 from socialModules.configMod import *
 # You need to:
 # pip install social-modules@git+https://git@github.com/fernand0/socialModules@dist
